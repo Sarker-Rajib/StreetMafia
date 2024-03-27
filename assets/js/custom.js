49c12e6d3c46a434f7468c3ba9426e43a38028ee
Rajib Sarker <redhat0593@gmail.com>
2024-03-27 20:08:50 +0600

    home page menu done

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+document.getElementById("statTrigger").addEventListener('click', () => {
+    const navMenu = document.getElementById("navMenu");
+    if (navMenu.classList.contains('show')) {
+        navMenu.classList.remove('show')
+    }
+    document.getElementById("stats").classList.add('show');
+});
+
+
+document.getElementById("statCloser").addEventListener('click', () => {
+    document.getElementById("stats").classList.remove('show')
+})
+
+
+document.getElementById("dropMenutrigger").addEventListener('click', () => {
+    const statmenu = document.getElementById("stats");
+    if (statmenu.classList.contains('show')) {
+        statmenu.classList.remove('show')
+    }
+    document.getElementById("navMenu").classList.add('show')
+})
+
+
+document.getElementById("dropMenuCloser").addEventListener('click', () => {
+    document.getElementById("navMenu").classList.remove('show')
+})
